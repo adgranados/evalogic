6741bcebdca1ac3e05cd35f38ec64a2a40641f7f
Daniel Granados <adgranados@gmail.com>
2013-11-13 14:54:44 -0500

    evalogic

---
+++ www/js/matriz.js
@@ -0,0 +1,193 @@
+//confiabilidad y eficiencia
+//comprencibilidad
+//portabilidad
+//Usabilidad y Operabilidad		 	
+matriz = {
+		//confiabilidad y eficiencia
+		"Alto":{
+			//comprensibilidad
+			"Alto":{
+				//portabilidad
+				"Alto":{
+					//Usabilidad y Operabilidad	
+					"Alto":"OPTIMA",
+					"Medio":"OPTIMA",
+					"Bajo":"OPTIMA"},
+				//portabilidad
+				"Medio":{
+					//Usabilidad y Operabilidad	
+					"Alto":"OPTIMA",
+					"Medio":"BUENO",
+					"Bajo":"BUENO"},
+				//portabilidad
+				"Bajo":{
+					//Usabilidad y Operabilidad	
+					"Alto":"BUENO",
+					"Medio":"Mejorable",
+					"Bajo":"Mejorable"}},
+			//comprensibilidad
+			"Medio":{
+				//portabilidad
+				"Alto":{
+					//Usabilidad y Operabilidad
+					"Alto":"OPTIMA",
+					"Medio":"OPTIMA",
+					"Bajo":"OPTIMA"},
+				//portabilidad
+				"Medio":{
+					//Usabilidad y Operabilidad
+					"Alto":"BUENO",
+					"Medio":"BUENO",
+					"Bajo":"BUENO"},
+				//portabilidad
+				"Bajo":{
+					//Usabilidad y Operabilidadd
+					"Alto":"Mejorable",
+					"Medio":"Mejorable",
+					"Bajo":"Mejorable"}},
+			//comprensibilidad
+			"Bajo":{
+				//portabilidad
+				"Alto":{
+					//Usabilidad y Operabilidad	
+					"Alto":"OPTIMA",
+					"Medio":"OPTIMA",
+					"Bajo":"BUENO"},
+				//portabilidad
+				"Medio":{
+					//Usabilidad y Operabilidad	
+					"Alto":"BUENO",
+					"Medio":"BUENO",
+					"Bajo":"Mejorable"},
+				//portabilidad
+				"Bajo":{
+					//Usabilidad y Operabilidad	
+					"Alto":"Mejorable",
+					"Medio":"Mejorable",
+					"Bajo":"A&M"}}},
+		//confiabilidad y eficiencia
+		"Medio":{
+			//comprensibilidad
+			"Alto":{
+				//portabilidad
+				"Alto":{
+					//Usabilidad y Operabilidad	
+					"Alto":"OPTIMA",
+					"Medio":"BUENO",
+					"Bajo":"BUENO"},
+				//portabilidad	
+				"Medio":{
+				    //Usabilidad y Operabilidad	
+					"Alto":"BUENO",
+					"Medio":"Mejorable",
+					"Bajo":"Mejorable"},
+				//portabilidad
+				"Bajo":{
+					//Usabilidad y Operabilidad	
+					"Alto":"Mejorable",
+					"Medio":"A&M",
+					"Bajo":"A&M"}},
+			//comprensibilidad
+			"Medio":{
+				//portabilidad
+					"Alto":{
+						//Usabilidad y Operabilidad	
+						"Alto":"BUENO",
+						"Medio":"BUENO",
+						"Bajo":"BUENO"},
+				//portabilidad
+					"Medio":{
+						//Usabilidad y Operabilidad	
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+				//portabilidad
+					"Bajo":{
+						//Usabilidad y Operabilidad	
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"A&M"}},
+			//comprensibilidad
+			"Bajo":{
+				//portabilidad
+					"Alto":{
+						//Usabilidad y Operabilidad	
+						"Alto":"BUENO",
+						"Medio":"BUENO",
+						"Bajo":"Mejorable"},
+				//portabilidad
+					"Medio":{
+						//Usabilidad y Operabilidad	
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"A&M"},
+				//portabilidad
+					"Bajo":{
+						//Usabilidad y Operabilidad	
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"A&M"}}
+				},
+		//confiabilidad y eficiencia
+		"Bajo":{
+				//comprensibilidad
+				"Alto":{
+					//portabilidad
+					"Alto":{
+						//Usabilidad y Operabilidad	
+						"Alto":"BUENO",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					//portabilidad
+					"Medio":{
+						//Usabilidad y Operabilidad	
+						"Alto":"Mejorable",
+						"Medio":"A&M",
+						"Bajo":"A&M"},
+					//portabilidad
+					"Bajo":{
+						//Usabilidad y Operabilidad	
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"Restructurar"}},
+				//comprensibilidad
+				"Medio":{
+					//portabilidad
+					"Alto":{
+						//Usabilidad y Operabilidad	
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					//portabilidad
+					"Medio":{
+						//Usabilidad y Operabilidad	
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"A&M"},
+					//portabilidad
+					"Bajo":{
+						//Usabilidad y Operabilidad	
+						"Alto":"A&M",
+						"Medio":"Restructurar",
+						"Bajo":"Restructurar"}},
+				//comprensibilidad
+				"Bajo":{
+					//portabilidad
+					"Alto":{
+						//Usabilidad y Operabilidad	
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"A&M"},
+					//portabilidad
+					"Medio":{
+						//Usabilidad y Operabilidad	
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"A&M"},
+					//portabilidad
+					"Bajo":{
+						//Usabilidad y Operabilidad	
+						"Alto":"Restructurar",
+						"Medio":"Restructurar",
+						"Bajo":"Restructurar"}}}
+	}
---
+++ www/js/matriz.old.js
@@ -0,0 +1,142 @@
+		 	
+//confiabilidad y eficiencia
+//comprencibilidad
+//portabilidad
+//Usabilidad y Operabilidad		 	
+matriz = {
+		//confiabilidad y eficiencia
+		"Alto":{
+			//comprencibilidad
+			"Alto":{
+				//portabilidad
+				"Alto":{
+					//Usabilidad y Operabilidad	
+					"Alto":"OPTIMA",
+					"Medio":"OPTIMA",
+					"Bajo":"BUENO"},
+				"Medio":{
+					//Usabilidad y Operabilidad	
+					"Alto":"BUENO",
+					"Medio":"BUENO",
+					"Bajo":"BUENO"},
+				"Bajo":{
+					//Usabilidad y Operabilidad	
+					"Alto":"A&M",
+					"Medio":"Mejorable",
+					"Bajo":"Mejorable"}},
+			"Medio":{
+				//portabilidad
+				"Alto":{
+					//Usabilidad y Operabilidad
+					"Alto":"OPTIMA",
+					"Medio":"OPTIMA",
+					"Bajo":"BUENO"},
+				"Medio":{
+					//Usabilidad y Operabilidad
+					"Alto":"Medio",
+					"Medio":"Medio",
+					"Bajo":"A&M"},
+				"Bajo":{
+					//Usabilidad y Operabilidadd
+					"Alto":"A&M",
+					"Medio":"Mejorable",
+					"Bajo":"Mejorable"}},
+			"Bajo":{
+				//portabilidad
+				"Alto":{
+					//Usabilidad y Operabilidad	
+					"Alto":"OPTIMA",
+					"Medio":"BUENO",
+					"Bajo":"BUENO"},
+				"Medio":{
+					//Usabilidad y Operabilidad	
+					"Alto":"BUENO",
+					"Medio":"Mejorable",
+					"Bajo":"A&M"},
+				"Bajo":{
+					//Usabilidad y Operabilidad	
+					"Alto":"Mejorable",
+					"Medio":"Mejorable",
+					"Bajo":"Mejorable"}}},
+		"Medio":{
+			"Alto":{
+				"Alto":{
+					"Alto":"BUENO",
+					"Medio":"BUENO",
+					"Bajo":"BUENO"},
+				"Medio":{
+					"Alto":"Mejorable",
+					"Medio":"Mejorable",
+					"Bajo":"Mejorable"},
+				"Bajo":{
+					"Alto":"Mejorable",
+					"Medio":"Mejorable",
+					"Bajo":"A&M"}},
+			"Medio":{
+					"Alto":{
+						"Alto":"BUENO",
+						"Medio":"BUENO",
+						"Bajo":"Mejorable"},
+					"Medio":{
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					"Bajo":{
+						"Alto":"Mejorable",
+						"Medio":"A&M",
+						"Bajo":"A&M"}},
+			"bajo":{
+					"Alto":{
+						"Alto":"BUENO",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					"Medio":{
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					"Bajo":{
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"A&M"}}
+				},
+		"Bajo":{
+				"Alto":{
+					"Alto":{
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					"Medio":{
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"A&M"},
+					"Bajo":{
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"Restructurar"}},
+				"Medio":{
+					"Alto":{
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					"Medio":{
+						"Alto":"Mejorable",
+						"Medio":"A&M",
+						"Bajo":"A&M"},
+					"Bajo":{
+						"Alto":"A&M",
+						"Medio":"Restructurar",
+						"Bajo":"Restructurar"}},
+				"Bajo":{
+					"Alto":{
+						"Alto":"Mejorable",
+						"Medio":"Mejorable",
+						"Bajo":"Mejorable"},
+					"Medio":{
+						"Alto":"A&M",
+						"Medio":"A&M",
+						"Bajo":"A&M"},
+					"Bajo":{
+						"Alto":"Restructurar",
+						"Medio":"Restructurar",
+						"Bajo":"Restructurar"}}}
+	}
